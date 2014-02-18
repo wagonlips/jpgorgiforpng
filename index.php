8b4a2e72646da6dbafc4f27dd68cc218b84de2c7
Sean Camden <sean@seancamden.com>
2014-02-17 18:24:24 -0800

    Added fonts, lobstertwo and am monkeying around with page layout, currently working with jquery mobile and trying to show images and captions horizontally.

--- index.php
+++ index.php
@@ -1,131 +1,124 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html> 
+<html>
 <head>
-<title>hello</title>
-<meta charset="utf-8">
-<?php include '/var/www/jpgorgiforpng/html/includes/funcs.php'; ?>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.1/jquery.js"></script>
-<style type="text/css">
-li {display:inline;float:left;}
-ul {width:840px;}
-</style>
-</head>
-<body>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1"> 
+  <title>web graphics for you</title> 
+  <?php include '/var/www/jpgorgiforpng/html/includes/funcs.php'; ?>
+  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.css" />
+  <style type="text/css">
+  .ui-content {max-width:760px;margin:0 auto;}
+  .images {display:inline;width:244px;height:136px;}
+  .caption {width:inherit;height:20px;}
+  </style>
+  <script src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+  <script src="http://code.jquery.com/mobile/1.1.1/jquery.mobile-1.1.1.min.js"></script>
+</head> 
+<body> 
+<!-- Page -->
 <?php $img00 = array('jay-peg-landscape.jpg','gif-landscape.gif','ping-landscape.png'); ?>
-<div class="content" id="box1">
-  <h1>web graphics for you</h1>
-  <div class="text">
+<div data-role="page" id="one">
+  <div data-role="header">
+    <h1>Hi, I'm Jay.</h1>
+  </div><!-- /header -->
+  <div data-role="content" >    
     <p>Hi, my name is Jay Peg. I&rsquo;m a Web graphic and these are my friends Gif and Ping.</p>
     <p>We can help your website look great, but you should know what our strengths and weaknesses are first.</p>
-  </div>
-  <ul class="images">
-    <li class="image jpg"><?php iDims($img00[0]); iSize($img00[0]);?></li>
-    <li class="image gif"><?php iDims($img00[1]); iSize($img00[1]);?></li>
-    <li class="image png"><?php iDims($img00[2]); iSize($img00[2]);?></li>
-  </ul>
-  <div class="text">
-  </div>
-</div><!-- .content #hello -->
+    <div class="imageBox">
+      <div class="images"><?php iDims($img00[0]); iSize($img00[0]);?></div>
+      <div class="images"><?php iDims($img00[1]); iSize($img00[1]);?></div>
+      <div class="images"><?php iDims($img00[2]); iSize($img00[2]);?></div>
+    </div>
+    <p><a href="#two" data-role="button">Show page "two"</a></p>        
+  </div><!-- /content -->
+  <div data-role="footer" data-theme="d">
+    <h4>Page Footer</h4>
+  </div><!-- /footer -->
+</div><!-- /page -->
 <?php $img01 = array('jay-peg-landscape.jpg','gif-landscape.gif','ping-landscape.png'); ?>
-<div class="content" id="box2" style="display:none;">
-  <h1>transparency</h1>
-  <div class="text">
-    <p>For instance, I can&rsquo;t do transparency; Gif can do it poorly; and Ping does it perfectly.</p>
-  </div>
-  <ul class="images">
-    <li class="image jpg"><?php iDims($img01[0]); iSize($img01[0]);?></li>
-    <li class="image gif"><?php iDims($img01[1]); iSize($img01[1]);?></li>
-    <li class="image png"><?php iDims($img01[2]); iSize($img01[2]);?></li>
-  </ul>
-  <div class="text">
-  </div>
-</div><!-- .content #hello -->
+<!-- Page -->
+<div data-role="page" id="two">
+  <div data-role="header">
+    <h1>Transparency</h1>
+  </div><!-- /header -->
+  <div data-role="content">      
+    <p>For instance, I can&rsquo;t do transparency, Gif can do it in a limited way, and Ping pretty much nails it.</p>
+    <ul class="images">
+      <li class="image jpg"><?php iDims($img01[0]); iSize($img01[0]);?></li>
+      <li class="image gif"><?php iDims($img01[1]); iSize($img01[1]);?></li>
+      <li class="image png"><?php iDims($img01[2]); iSize($img01[2]);?></li>
+    </ul>
+    <p><a href="#one" data-direction="reverse" data-role="button">Back to page "one"</a></p>     
+    <p><a href="#three" data-role="button">Show page "three"</a></p>     
+  </div><!-- /content -->
+  <div data-role="footer" data-theme="d">
+    <h4>Page Footer</h4>
+  </div><!-- /footer -->
+</div><!-- /page -->
+<!-- Page -->
 <?php $img02 = array('photo-landscape.jpg','photo-landscape.gif','photo-landscape.png'); ?>
-<div class="content" id="box3" style="display:none;">
-  <h1>display photo art</h1>
-  <div class="text">
+<div data-role="page" id="three">
+  <div data-role="header">
+    <h1>display photo art</h1>
+  </div><!-- /header -->
+  <div data-role="content" >    
     <p>If you want to display a photo, I&rsquo;m probably your best choice. That&rsquo;s because I can render millions of colors and show a reasonably sharp image more cheaply than my friends Gif and Ping. I can also employ <a href="https://en.wikipedia.org/wiki/Lossy_compression">lossy compression</a>, something my friends can&rsquo;t do.</p>
     <p>As you can see, I&rsquo;m the lightest graphic. Your visitors will be able to download me faster than my friends. Also, Gif looks terrible because she can&rsquo;t do as many colors. Ping can also do millions of colors, but at a much greater cost.</p>
-  </div>
-  <ul class="images">
-    <li class="image jpg"><?php iDims($img02[0]); iSize($img02[0]);?></li>
-    <li class="image gif"><?php iDims($img02[1]); iSize($img02[1]);?></li>
-    <li class="image png"><?php iDims($img02[2]); iSize($img02[2]);?></li>
-  </ul>
-  <div class="text">
-  </div>
-</div><!-- .content #photoArt -->
+    <ul class="images">
+      <li class="image jpg"><?php iDims($img02[0]); iSize($img02[0]);?></li>
+      <li class="image gif"><?php iDims($img02[1]); iSize($img02[1]);?></li>
+      <li class="image png"><?php iDims($img02[2]); iSize($img02[2]);?></li>
+    </ul>
+    <p><a href="#two" data-direction="reverse" data-role="button">Back to page "two"</a></p>     
+    <p><a href="#four" data-role="button">Show page "four"</a></p>        
+  </div><!-- /content -->
+  <div data-role="footer" data-theme="d">
+    <h4>Page Footer</h4>
+  </div><!-- /footer -->
+</div><!-- /page -->
 <?php $img03 = array('gov-usda-xl.jpg','gov-usda-xl.gif','gov-usda-xl.png'); ?>
-<div class="content" id="box4" style="display:none;">
-  <h1>display vector art</h1>
-  <div class="text">
+<!-- Page -->
+<div data-role="page" id="four">
+  <div data-role="header">
+    <h1>Transparency</h1>
+  </div><!-- /header -->
+  <div data-role="content">      
     <p>Why not use me for everything? Because I&rsquo;m actually very bad at some things that my friends do very well.</p>
     <p>For graphics with fewer than 256 colors, particularly ones created in a vector art program like <a href="http://inkscape.org/en/">Inkscape</a>, Gif works best. She&rsquo;s more lightweight than me and has cleaner edges.</p>
-  </div>
-  <ul class="images">
-    <li class="image jpg"><?php iDims($img03[0]); iSize($img03[0]);?></li>
-    <li class="image gif"><?php iDims($img03[1]); iSize($img03[1]);?></li>
-    <li class="image png"><?php iDims($img03[2]); iSize($img03[2]);?></li>
-    <p class="caption">logo by <a href="https://github.com/seanherron/Gov-Webicons">Sean Herron</a></p>
-  </ul>
-  <div class="text">
+    <ul class="images">
+      <li class="image jpg"><?php iDims($img03[0]); iSize($img03[0]);?></li>
+      <li class="image gif"><?php iDims($img03[1]); iSize($img03[1]);?></li>
+      <li class="image png"><?php iDims($img03[2]); iSize($img03[2]);?></li>
+    </ul>
     <p>Gif beats Ping and I on size by actually throwing out colors she&rsquo;s not using. In this case, all but 16 colors were discarded.</p>
     <p>Ping is also terrific at stuff like this, though he might cost a little more. He does <a href="#">transparency</a> better, too. </p>
-  </div>
-</div><!-- .content #vectorArt -->
+    <p><a href="#three" data-direction="reverse" data-role="button">Back to page "three"</a></p>     
+    <p><a href="#five" data-role="button">Show page "five"</a></p>        
+  </div><!-- /content -->
+  <div data-role="footer" data-theme="d">
+    <h4>Page Footer</h4>
+  </div><!-- /footer -->
+</div><!-- /page -->
 <?php $img04 = array('gov-usda-xl.jpg','gov-usda-xl.gif','gov-usda-xl.png'); ?>
-<div class="content" id="box5" style="display:none;">
-  <h1>gradients</h1>
-  <div class="text">
+<!-- Page -->
+<div data-role="page" id="five">
+  <div data-role="header">
+    <h1>words in text</h1>
+  </div><!-- /header -->
+  <div data-role="content">      
     <p>One thing you shouldn&rsquo;t do with an image is words. Search engines are your bullhorn on the Web. Search engines read text. So, if you put your words inside an image, it&rsquo;s just like you&rsquo;re whispering.</p>
     <p>Also, by putting words in an image, you no longer have a good image choice. I will put artifacts around the edges of your type unless you turn off most or all of my compression, losing the cost savings I usually bring. Gif displays text well, provided you stay within her limitations (no more than 256 colors). And Ping can do text well, but he&rsquo;s expensive.</p>
-  </div>
-  <ul class="images">
-    <li class="image jpg"><?php iDims($img04[0]); iSize($img04[0]);?></li>
-    <li class="image gif"><?php iDims($img04[1]); iSize($img04[1]);?></li>
-    <li class="image png"><?php iDims($img04[2]); iSize($img04[2]);?></li>
-    <p class="caption"></p>
-  </ul>
-  <div class="text">
-    <p>Gif beats Ping and I by actually throwing out colors she&rsquo;s not using. In this case, all but 16 colors were discarded.</p>
-  </div>
-</div><!-- .content #vectorArt -->
-<?php $img05 = array('gov-usda-xl.jpg','gov-usda-xl.gif','gov-usda-xl.png'); ?>
-<div class="content" id="box6" style="display:none;">
-  <h1>new box</h1>
-  <div class="text">
-    <p>One thing you shouldn&rsquo;t do with an image is words. Search engines are your bullhorn on the Web. Search engines read text. So, if you put your words inside an image, it&rsquo;s just like you&rsquo;re whispering.</p>
-    <p>Also, by putting words in an image, you no longer have a good image choice. I will put artifacts around the edges of your type unless you turn off most or all of my compression, losing the cost savings I usually bring. Gif displays text well, provided you stay within her limitations (no more than 256 colors). And Ping can do text well, but he&rsquo;s expensive.</p>
-  </div>
-  <ul class="images">
-    <li class="image jpg"><?php iDims($img05[0]); iSize($img05[0]);?></li>
-    <li class="image gif"><?php iDims($img05[1]); iSize($img05[1]);?></li>
-    <li class="image png"><?php iDims($img05[2]); iSize($img05[2]);?></li>
-    <p class="caption"></p>
-  </ul>
-  <div class="text">
-    <p>Gif beats Ping and I by actually throwing out colors she&rsquo;s not using. In this case, all but 16 colors were discarded.</p>
-  </div>
-</div><!-- .content #vectorArt -->
-<button class="arrow" id="dec">&lt;</button>
-<button class="arrow" id="inc">&gt;</button>
-<script type="text/javascript">
-var numDivs = $('.content').length
-var incValue = 1;
-$( "#inc" ).click(function() {
-  if (incValue < numDivs) {
-    $( "#box" + incValue ).hide( "slow" );
-    incValue++;
-    $( "#box" + incValue ).show( "slow" );
-  }
-});
-$( "#dec" ).click(function() {
-  if (incValue > 1) {
-    $( "#box" + incValue ).hide( "slow" );
-    incValue--;
-    $( "#box" + incValue ).show( "slow" );
-  }
-});
-</script>
+    <ul class="images">
+      <li class="image jpg"><?php iDims($img04[0]); iSize($img04[0]);?></li>
+      <li class="image gif"><?php iDims($img04[1]); iSize($img04[1]);?></li>
+      <li class="image png"><?php iDims($img04[2]); iSize($img04[2]);?></li>
+    </ul>
+    <p><a href="#four" data-direction="reverse" data-role="button">Back to page "four"</a></p>     
+    <p><a href="#six" data-role="button">Show page "six"</a></p>        
+  </div><!-- /content -->
+  <div data-role="footer" data-theme="d">
+    <h4>Page Footer</h4>
+  </div><!-- /footer -->
+</div><!-- /page -->
 </body>
 </html>
